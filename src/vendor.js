88571efc35bdb9f53caacf83ecbf71ce1efd28c3
kumudug <kumudug1@gmail.com>
2017-06-04 18:20:51 +1000

    initial try for webpack

---
+++ src/vendor.js
@@ -0,0 +1,17 @@
+"use strict";
+// Angular 4
+require("@angular/platform-browser");
+require("@angular/platform-browser-dynamic");
+require("@angular/core");
+require("@angular/common");
+require("@angular/http");
+require("@angular/router");
+require("@angular/compiler");
+require("@angular/forms");
+require("angular-in-memory-web-api");
+//import 'jQuery';
+//import 'bootstrap';
+require("rxjs");
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+//# sourceMappingURL=vendor.js.map
---
+++ src/vendor.ts
@@ -0,0 +1,18 @@
+// Angular 4
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+import '@angular/compiler';
+import '@angular/forms';
+
+import 'angular-in-memory-web-api';
+//import 'jQuery';
+//import 'bootstrap';
+
+import 'rxjs';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
