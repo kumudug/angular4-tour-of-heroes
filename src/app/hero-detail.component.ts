505f82afea76209c83ece25f9c19950d2c48a1f1
kumudug <kumudug1@gmail.com>
2017-06-04 18:28:02 +1000

    mfix hero-detail.component

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -4,6 +4,7 @@ import { Location }                 from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 
 import { HeroService } from './hero.service';
+import { Hero } from './hero';
 
 @Component({
   selector: 'hero-detail',
@@ -12,6 +13,7 @@ import { HeroService } from './hero.service';
 
 })
 export class HeroDetailComponent implements OnInit {
+    hero: Hero;
     constructor(
       private heroService: HeroService,
       private route: ActivatedRoute,
