ed931eab3dd74e51d5e581d343a906b5ece3f117
kumudug <kumudug1@gmail.com>
2017-11-03 16:08:09 +1100

    HTTP service to get heroes with in memory web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -14,6 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService), //Rather than require a real API server, this example simulates communication with the remote server by adding the InMemoryWebApiModule to the module imports, effectively replacing the Http client's XHR backend service with an in-memory alternative.
     AppRoutingModule
   ],
   declarations: [
